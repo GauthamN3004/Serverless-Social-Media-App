fbfed137fd193c9675fd9fa07b3a8d9c10f9b216
GauthamN3004 <gauthamn3004@gmail.com>
2024-02-01 08:30:57 +0530

    adding user CRUD handlers

---
+++ serverless-social-media-app/handler.js
@@ -0,0 +1,117 @@
+'use strict';
+const dynamoDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new dynamoDB.DocumentClient({region: 'ap-south-1'});
+const { v4: uuidv4 } = require('uuid');
+
+module.exports.createUser = async (event, context, cb) => {
+	let event_data = JSON.parse(event.body);
+	const tableName = process.env.MAIN_TABLE;
+	let userId = uuidv4();
+	try{
+		const params = {
+			TableName : tableName,
+			Item: {
+			   PK: `USER#ID#${userId}`,
+			   SK: `USER#UNAME#${event_data.username}`,
+			   firstname: event_data.firstname
+			},
+			ConditionExpression: 'attribute_not_exists(SK)'
+		};
+
+		await documentClient.put(params).promise();
+		cb(null, {
+			statusCode: 201,
+			body: JSON.stringify({
+				status: 'success',
+				message: 'User created succesfully.',
+			}),
+	    });
+	} catch(error) {
+		cb(null, {
+			statusCode: 500,
+			body: JSON.stringify({
+				status: 'error',
+				message: error.message,
+			}),
+	    });
+	}
+
+};
+
+module.exports.updateUser = async (event, context, cb) => {
+	let event_data = JSON.parse(event.body);
+	let userId = event.pathParameters.id;
+	const tableName = process.env.MAIN_TABLE;
+	try{
+		let pk = `USER#ID#${userId}`;
+		let sk = `USER#UNAME#${event_data.username}`;
+		var params = {
+			TableName: tableName,
+			Key: { PK: pk, SK: sk },
+			UpdateExpression: 'set #att1 = :val1, #att2 = :val2',
+			ExpressionAttributeNames: {'#att1': 'firstname', '#att2': 'lastname'},
+			ExpressionAttributeValues: {':val1' : event_data.firstname, ':val2' : event_data.lastname },
+			ConditionExpression: 'attribute_exists(PK) AND attribute_exists(SK)'
+		};
+		await documentClient.update(params).promise();
+		cb(null, {
+			statusCode: 200,
+			body: JSON.stringify({
+				status: 'success',
+				message: 'User data updated successfully.',
+			}),
+	    });
+	} catch(error) {
+		cb(null, {
+			statusCode: 500,
+			body: JSON.stringify({
+				status: 'error',
+				message: error.message,
+			}),
+	    });
+	}
+};
+
+module.exports.getUserById = async (event) => {
+	let userId = event.pathParameters.id;
+	return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      status: 'success',
+      message: `User ${userId} updated`,
+    }),
+  };
+};
+
+module.exports.deleteUser = async (event, context, cb) => {
+	let event_data = JSON.parse(event.body);
+	const tableName = process.env.MAIN_TABLE;
+	try{
+		let pk = `USER#ID#${event.pathParameters.id}`;
+		let sk = `USER#UNAME#${event_data.username}`;
+		var params = {
+			TableName: tableName,
+			Key: { PK: pk, SK: sk },
+			ConditionExpression: 'attribute_exists(PK) AND attribute_exists(SK)'
+		};
+		await documentClient.delete(params).promise();
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				status: 'success',
+				message: `User deleted successfully.`,
+			})
+		};
+	} catch(error) {
+		cb(null, {
+			statusCode: 500,
+			body: JSON.stringify({
+				status: 'error',
+				message: error.message,
+			}),
+	    });
+	}
+};
